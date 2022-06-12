ff6094349e15daf63a092c1ad39274212cee5dd1
Gabi <pagameri@gmail.com>
2022-06-12 13:40:17 +0100

    Fix visibility, btn containers, events
    Use containers for visibility, hide/unhide
    
    Wanted to use display: block but block doesn't work with flex so changed it to visibility. Like this the boxes don't get swapped
    
    Fixed the issue of having to iterate over the contents of the button containers while also taking away the reset button container as for once it is not needed and for second it was in the way and did not allow to add height to it for some reason.
    
    Also made elements hide and show up at different events.
    
    Added extra messages that show up for certain events
    
    Changed text color in many places depending on the "result"

--- ui.js
+++ ui.js
@@ -1,10 +1,12 @@
 const buttons = document.querySelectorAll('.button-container');
 const round = document.getElementById('round');
+const tracker = document.querySelector('.tracker');
 const message = document.getElementById('message');
 const player = document.getElementById('player');
 const computer = document.getElementById('computer');
 const final = document.getElementById('final');
-const resetContainer = document.querySelectorAll('.reset-container');
+const resetBtn = document.getElementById('reset');
+const buttonContainer = document.querySelector('.button-container')
 
 let computerSelection;
 let playerSelection;
@@ -15,33 +17,27 @@ let rounds = 0;
 buttons.forEach((button) => {
   button.addEventListener('click', function (e) { 
     playerSelection = e.target.id;
-
+    final.textContent = "";
+    tracker.style.visibility = "visible";
     round.textContent = `Round: ${rounds + 1}`;
     game();
     });
 });
 
-resetContainer.forEach((item) => {
-  item.addEventListener('click', e => {
-    buttons.forEach((button) => {
-      button.style.display="block";
-    });
-    resetContainer.forEach((item) => {
-      item.style.display="none";
-    });
-    computerSelection = 0;
-    playerSelection = 0;
-    playerScore = 0;
-    computerScore = 0;
-    rounds = 0;
-    round.textContent = "Round:";
-    player.textContent = "Player:";
-    computer.textContent = "Computer:"
-    final.textContent = "";
-  });
-})
-
-
+resetBtn.addEventListener('click', e => {
+  buttonContainer.style.visibility="visible";
+  resetBtn.style.visibility="hidden";
+  computerSelection = 0;
+  playerSelection = 0;
+  playerScore = 0;
+  computerScore = 0;
+  rounds = 0;
+  round.textContent = "Round:";
+  player.textContent = "Player:";
+  computer.textContent = "Computer:"
+  message.textContent = "";
+  final.textContent = "";
+});
 
 function computerPlay () {
   let options = ["rock", "paper", "scissors"];
@@ -54,24 +50,32 @@ function playRound(playerSelection, computerSelection) {
 
   if (playerSelection === computerSelection) {
     message.textContent = "It's a tie!";
+    message.style.color="black";
     return ("tie");
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     message.textContent = "You Win! Rock crushes scissors";
+    message.style.color="green";
     return ("player");
   } else if (computerSelection === "rock" && playerSelection === "scissors") {
     message.textContent = "You Lose! Rock crushes scissors";
+    message.style.color="red";
     return ("computer");
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     message.textContent = "You Win! Paper covers rock";
+    message.style.color="green";
     return ("player");
   } else if (computerSelection === "paper" && playerSelection === "rock") {
     message.textContent = "You Lose! Paper covers rock";
+    message.style.color="red";
     return ("computer");
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     message.textContent = "You Win! Scissors cut paper";
+    message.style.color="green";
     return ("player");
   } else if (computerSelection === "scissors" && playerSelection === "paper") {
     message.textContent = "You Lose! Scissors cut paper";
+    message.style.color="red";
+    message.style.color="green";
     return ("computer");
   } else {
     message.textContent = "It went terribly wrong";
@@ -81,42 +85,37 @@ function playRound(playerSelection, computerSelection) {
 
 function game() {
   let winner = playRound(playerSelection, computerSelection);
-  
+  player.textContent = `Player: ${playerScore}`;
+  computer.textContent = `Computer: ${computerScore}`;
 
   if (winner === "player") {
     playerScore++;
     player.textContent = `Player: ${playerScore}`;
-  }
-
-  else if (winner === "computer") {
+  } else{ 
     computerScore++;
     computer.textContent = `Computer: ${computerScore}`;
-  }
+  } 
   
   if (playerScore === 5 || computerScore === 5) {
     result();
-    swapBoxes();
-    buttons.forEach((button) => {
-      button.style.display="none";
-    });
-    resetContainer.forEach((item) => {
-      item.style.visibility="visible";
-    });
+    buttonContainer.style.visibility="hidden";
+    resetBtn.style.visibility="visible";
   } else {
     rounds++;
   }
 }
 
-function swapBoxes() {
-  
-}
 
 function result() {
   if (playerScore > computerScore) {
-    final.textContent = "YOU WIN";
-  } else if (computerScore > playerScore) {
-    final.textContent = "GAME OVER!";
+    final.textContent = "YOU WIN :)";
+    final.style.color="green";
   } else {
-    final.textContent = "IT'S A TIE!"; 
+    final.textContent = "GAME OVER :(";
+    final.style.color="red";
   }
+  message.textContent = "Click reset to play new game!";
+  message.style.color = "blue";
 }
+
+
